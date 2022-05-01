4e9d3cbf7c463c733e142ae093c3b72232c756ff
minjeongPark41 <79864818+minjeongPark41@users.noreply.github.com>
2022-05-01 13:25:46 +0900

    테스트 우선 개발 - 데이터 일일히 작성했을 경우

--- text-refiner/index.js
+++ text-refiner/index.js
@@ -1,6 +1,12 @@
 // 중복된 문자를 줄여주는지를 테스트
 function refineTest(s) {
-  return s.replace("    ", " ").replace("  ", " ");
+  return s.replace("    ", " ")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("\t", "")
+    .replace("mockist", "*******")
+    .replace("purist", "******")
 }
 
 module.exports = refineTest;
--- text-refiner/index.test.js
+++ text-refiner/index.test.js
@@ -7,7 +7,27 @@ test.each([
   ["hello  world", "hello world"],
   ["hello   world", "hello world"],
   ["hello    world", "hello world"],
+  ["hello     world", "hello world"],
+  ["hello      world", "hello world"],
+  ["hello       world", "hello world"],
 ])('parameterized 테스트코드 작성 -  test.each 사용', (first, expected) => {
   const actual = sut(first);
+  expect(actual).toBe(expected);
+})
+
+test.each([
+  ["hello\t world", "hello world"],
+  ["hello \tworld", "hello world"],
+])('테스트 우선 개발1', (first, expected) => {
+  const actual = sut(first);
+  expect(actual).toBe(expected);
+})
+
+test.each([
+  ["hello mockist", ["mockist", "purist"], "hello *******"],
+  ["hello purist", ["mockist", "purist"], "hello ******"],
+])('테스트 우선 개발2', (first, second, expected) => {
+  const options = {second}; // {options : ["mockist", "purist"]}
+  const actual = sut(first, options);
   expect(actual).toBe(expected);
 })
