613e5c95a344bd00712add48f42002ccd440fd84
minjeongPark41 <79864818+minjeongPark41@users.noreply.github.com>
2022-05-01 12:49:14 +0900

    중복된 문자를 줄여주는 함수 테스트코드 - parameterized 테스트코드 작성 (test.each 사용)

--- text-refiner/index.test.js
+++ text-refiner/index.test.js
@@ -2,10 +2,12 @@ const sut = require("./index");
 // sut의 약자는 system under test
 // 대상이 되는 코드를 불러와서 변수 sut에 담은 것
 
-// for loop를 사용하여 반복 줄이기
-test('중복된 공백을 하나의 공백으로 줄이는지를 확인하는지 테스트 - for loop 사용', ()=>{
-  for (const source of ['hello  world', 'hello    world', 'hello   world']){
-    const actual = sut(source);
-    expect(actual).toBe("hello world");
-  }
+// parameterized 테스트코드 작성 -  test.each 사용
+test.each([
+  ["hello  world", "hello world"],
+  ["hello   world", "hello world"],
+  ["hello    world", "hello world"],
+])('parameterized 테스트코드 작성 -  test.each 사용', (first, expected) => {
+  const actual = sut(first);
+  expect(actual).toBe(expected);
 })
